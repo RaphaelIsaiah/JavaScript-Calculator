657c8acf4aae5c13987229ddf81611bd6013747c
RaphaelIsaiah <rafvie12@gmail.com>
2025-02-08 17:06:31 +0100

    implement calculator logic with expression evaluation and update display component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,17 @@
-const Display = () => {
-  return <div id="display">Display</div>;
+import PropTypes from "prop-types";
+
+const Display = ({ expression, result }) => {
+  return (
+    <div id="display">
+      <div>{expression}</div>
+      <div>{result}</div>
+    </div>
+  );
+};
+
+Display.propTypes = {
+  expression: PropTypes.string.isRequired,
+  result: PropTypes.string.isRequired,
 };
 
 export default Display;
